594d686751c01f75697e7cb5e44b9b59f1942cca
nilvercor-crypto <nilvero@gmail.com>
2026-07-02 22:46:14 -0500

    servicios

--- src/services/proyectoService.js
+++
@@ -1,9 +0,0 @@
-const URL = "http://localhost:3000/proyectos";
-export async function obtenerProyectos(){
-
-const response = await fetch(URL);
-
-const data = await response.json();
-
-return data;
-}
